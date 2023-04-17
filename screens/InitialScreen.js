23d69d3aa87062c18265bbba496e7f531d846d15
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-04-18 00:53:56 +0200

    feat: config initial stack

--- screens/InitialScreen.js
+++ screens/InitialScreen.js
@@ -1,12 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 
-const InitialScreen = () => {
+const InitialScreen = ({ navigation }) => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>InitialScreen</Text>
+			<TouchableOpacity onPress={() => navigation.navigate("Login")}>
+				<Text>Go to Login</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
 
 export default InitialScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "orange",
+	},
+});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 const LoginScreen = () => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>LoginScreen</Text>
 		</View>
 	);
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		backgroundColor: "pink",
+	},
+});
