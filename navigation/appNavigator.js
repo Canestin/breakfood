4ce54594db6805753239c95fd283c3f203278420
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-05-11 12:34:14 +0200

    refactor: enable switch to productTourScreen

--- navigation/appNavigator.js
+++ navigation/appNavigator.js
@@ -4,25 +4,33 @@ import { useColorScheme } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { InitialScreen, LoginScreen } from "../screens";
+import ProductTourScreen from "../screens/ProductTourScreen";
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
-	const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme();
 
-	return (
-		<NavigationContainer>
-			<StatusBar style={colorScheme === "light" ? "dark" : "light"} />
-			<Stack.Navigator initialRouteName="Initial">
-				<Stack.Screen
-					name="Initial"
-					component={InitialScreen}
-					options={{
-						headerShown: false,
-					}}
-				/>
-				<Stack.Screen name="Login" component={LoginScreen} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <StatusBar style={colorScheme === "light" ? "dark" : "light"} />
+      <Stack.Navigator initialRouteName="Initial">
+        <Stack.Screen
+          name="Initial"
+          component={InitialScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="ProductTour"
+          component={ProductTourScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- screens/InitialScreen.js
+++ screens/InitialScreen.js
@@ -2,23 +2,23 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 
 const InitialScreen = ({ navigation }) => {
-	return (
-		<View style={styles.container}>
-			<Text>InitialScreen</Text>
-			<TouchableOpacity onPress={() => navigation.navigate("Login")}>
-				<Text>Go to Login</Text>
-			</TouchableOpacity>
-		</View>
-	);
+  return (
+    <View style={styles.container}>
+      <Text>InitialScreen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("ProductTour")}>
+        <Text>Go to Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default InitialScreen;
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		backgroundColor: "orange",
-	},
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
+  },
 });
