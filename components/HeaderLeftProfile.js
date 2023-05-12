10c49c93e411fd05ebfbc031dad170aa25565e72
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-05-12 19:55:54 +0200

    fix(component): add header left profile

---
+++ components/HeaderLeftProfile.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { StyleSheet, View, Pressable, Image } from "react-native";
+import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+
+const HeaderLeftProfile = () => {
+  const { avatar, color } = useSelector((state) => state.avatar);
+  const navigation = useNavigation();
+  return (
+    <View>
+      <Pressable onPress={() => navigation.navigate("Profile")}>
+        <Image
+          style={[styles.img, { backgroundColor: color ? color : "white" }]}
+          source={avatar}
+        />
+      </Pressable>
+    </View>
+  );
+};
+
+export default HeaderLeftProfile;
+
+const styles = StyleSheet.create({
+  img: {
+    width: 35,
+    height: 35,
+    marginLeft: 12,
+    borderRadius: 35 / 2,
+    borderWidth: 0.1,
+    borderColor: "gray",
+  },
+});
