a3018a7420b498dbbc71fe62b1ca63fe4f82c911
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-05-12 19:59:22 +0200

    chore: add screen to index design pattern to export screen

--- screens/index.js
+++ screens/index.js
@@ -1,5 +1,17 @@
 import InitialScreen from "./InitialScreen";
 import LoginScreen from "./LoginScreen";
 import ProductTourScreen from "./ProductTourScreen";
+import SetupScreen from "./SetupScreen";
+import HomeScreen from "./HomeScreen";
+import SearchScreen from "./SearchScreen";
+import ProfileScreen from "./ProfileScreen";
 
-export { InitialScreen, LoginScreen, ProductTourScreen };
+export {
+  InitialScreen,
+  LoginScreen,
+  ProductTourScreen,
+  SetupScreen,
+  HomeScreen,
+  SearchScreen,
+  ProfileScreen,
+};
