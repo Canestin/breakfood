a48b418a649fc6df5a955e7ec85b306a92d9514b
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-05-12 19:56:38 +0200

    chore(data): add nutritionists

---
+++ data/NutritionistData.js
@@ -0,0 +1,31 @@
+const NUTRITIONISTDATA = [
+  {
+    key: "N1",
+    firstname: "Antonio",
+    recipes: 89,
+    image: require("../assets/images/nutritionist1.png"),
+  },
+
+  {
+    key: "N2",
+    firstname: "Santana",
+    recipes: 57,
+    image: require("../assets/images/nutritionist2.png"),
+  },
+
+  {
+    key: "N3",
+    firstname: "John",
+    recipes: 83,
+    image: require("../assets/images/nutritionist3.png"),
+  },
+
+  {
+    key: "N4",
+    firstname: "Jada",
+    recipes: 72,
+    image: require("../assets/images/nutritionist4.png"),
+  },
+];
+
+export default NUTRITIONISTDATA;
