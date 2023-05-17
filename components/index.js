d2296174dd1b28ccd991e73b1b9170b94a8e7e98
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-05-17 09:42:34 +0200

    refactor(component): add index design pattern to export all components

---
+++ components/index.js
@@ -0,0 +1,31 @@
+import BackButton from "./BackButton";
+import Category from "./Category";
+import CustomHeader from "./CustomHeader";
+import CustomTextInput from "./CustomTextInput";
+import ExternalLink from "./ExternalLink";
+import HeaderLeftProfile from "./HeaderLeftProfile";
+import NumberText from "./NumberText";
+import Nutritionist from "./Nutritionist";
+import Post from "./Post";
+import SearchBar from "./SearchBar";
+import Recipe from "./Recipe";
+import SubmitButton from "./SubmitButton";
+import Username from "./Username";
+import FollowerItem from "./FollowerItem";
+
+export {
+  BackButton,
+  Category,
+  CustomHeader,
+  CustomTextInput,
+  ExternalLink,
+  HeaderLeftProfile,
+  NumberText,
+  Nutritionist,
+  Post,
+  SearchBar,
+  Recipe,
+  SubmitButton,
+  Username,
+  FollowerItem,
+};
