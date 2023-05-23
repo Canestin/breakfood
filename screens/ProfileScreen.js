3783bf0da9fa0547c64cd5fe8ab38335d1fa7bae
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-05-23 21:48:23 +0200

    feat(screen): add recipes component to profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import userPic from "../assets/images/userpic1.jpeg";
@@ -6,19 +6,31 @@ import banner from "../assets/images/banner.png";
 import { Image } from "react-native";
 import Colors from "../constants/Colors";
 import NumberText from "../components/NumberText";
+import Recipe from "../components/Recipe";
+import { multiplePostData } from "../data/FakePostData";
+
+const { height, width } = Dimensions.get("window");
+
 const ProfileScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <View style={styles.profileContainer}>
         <View style={styles.profileBanner}>
           <Image source={banner} style={{ width: "100%", height: "100%" }} />
         </View>
         <View style={styles.profileInfo}>
           <View style={styles.profileInfoText}>
-            <Text style={{ fontSize: 20, color: Colors.primary }}>
-              Pierro236{" "}
+            <Text
+              style={{
+                fontSize: 20,
+                color: Colors.primary,
+                fontWeight: "bold",
+                marginTop: 5,
+              }}
+            >
+              Pierre Sedo
             </Text>
             <Text style={{ fontSize: 15, color: Colors.black }}>
               Paris, France
@@ -45,7 +57,12 @@ const ProfileScreen = () => {
           />
         </View>
       </View>
-    </View>
+      <View style={styles.recipesContainer}>
+        {multiplePostData.map((item, i) => (
+          <Recipe key={i} style={styles.item} recipe={item} />
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -55,43 +72,50 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  profileContainer: {
-    width: "100%",
-    height: 200,
-  },
+  profileContainer: {},
   profileBanner: {
-    width: "100%",
-    height: "70%",
+    height: height * 0.4 * 0.35,
     borderRadius: 12,
   },
   profilePicture: {
-    width: 120,
-    height: 120,
-    borderRadius: 120 / 2,
+    height: height * 0.4 * 0.35,
+    aspectRatio: 1,
     objectFit: "cover",
     position: "absolute",
-    top: "30%",
-    left: "35%",
+    top: height * 0.4 * 0.175,
+    left: (width - height * 0.4 * 0.35) / 2,
   },
   profileInfo: {
     width: "100%",
-    height: 200,
-    padding: 25,
+    paddingHorizontal: "5%",
+    paddingTop: height * 0.4 * 0.175,
   },
   profileInfoText: {
-    width: "100%",
-    height: "50%",
     alignItems: "center",
     justifyContent: "center",
   },
   profileInfoData: {
-    width: "100%",
-    height: "50%",
+    marginTop: 10,
     backgroundColor: Colors.primaryOpacity,
     borderRadius: 12,
     padding: 20,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  recipesContainer: {
+    flex: 1,
+    marginHorizontal: 20,
+    gap: 10,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginVertical: 30,
+  },
+  item: {
+    width: Dimensions.get("window").width / 2 - 25,
+    height: 250,
+    backgroundColor: "lightgrey",
+    borderRadius: 20,
+    overflow: "hidden",
   },
 });
