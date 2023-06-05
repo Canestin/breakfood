a2a954e583f81ac92b5308f4467fde57473ee83f
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2023-06-05 14:20:22 +0200

    chore: prettier formatting

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import AppNavigator from "./navigation/appNavigator";
 
-export default function App() {
-	return (
-		<Provider store={store}>
-			<AppNavigator />
-		</Provider>
-	);
-}
+const App = () => {
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
+};
+
+export default App;
